08e8df8a6bb5c03dd34cc6bf25bc084e58272d2b
owenosga <owenosga@outlook.com>
2023-12-06 16:25:52 -0600

    completed work on Google Maps implementation

--- app/src/main/java/com/cs407/madcal/MapFragment.java
+++ app/src/main/java/com/cs407/madcal/MapFragment.java
@@ -1,48 +1,97 @@
 package com.cs407.madcal;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.Toast;
+import android.Manifest;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.TypeFilter;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 
-import java.io.IOException;
-import java.util.List;
+import java.util.Arrays;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private String wiscId; // Add this variable
+    private PlacesClient placesClient;
+    private static final String TAG = "MapFragment";
 
-    @Nullable
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
 
-        // Retrieve the WISC ID from the fragment's arguments
-        if (getArguments() != null) {
-            wiscId = getArguments().getString("WISC_ID");
+        // Initialize the Places SDK
+        if (!Places.isInitialized()) {
+            Places.initialize(requireContext(), "AIzaSyDlyJg8OpAM2n5HLmuxH4pOfH_LUpMXbmc");
         }
+        placesClient = Places.createClient(getContext());
+
+        // Initialize the AutocompleteSupportFragment
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        // Specify the types of place data to return
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG));
+        autocompleteFragment.setTypeFilter(TypeFilter.ADDRESS);
+
+        // Set up a PlaceSelectionListener to handle the response
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                // Move the camera to the selected place
+                LatLng location = place.getLatLng();
+                if (location != null) {
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15));
+
+                    // Add a marker and set the title as the place name
+                    mMap.addMarker(new MarkerOptions()
+                            .position(location)
+                            .title(place.getName())
+                            .snippet(place.getAddress())); // Optional: You can also show the address as a snippet
+
+                    // Optional: If you want to show an info window immediately
+                    mMap.setOnMarkerClickListener(marker -> {
+                        marker.showInfoWindow();
+                        return true;
+                    });
+                }
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
+                // Handle the error
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
 
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
                 .findFragmentById(R.id.map);
@@ -50,67 +99,41 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
             mapFragment.getMapAsync(this);
         }
 
-        EditText searchEditText = view.findViewById(R.id.searchEditText);
-        searchEditText.setOnEditorActionListener((v, actionId, event) -> {
-            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                performSearch(v.getText().toString());
-                return true;
-            }
-            return false;
-        });
-
-        ImageButton locateButton = view.findViewById(R.id.locateButton);
-        locateButton.setOnClickListener(v -> centerMapOnCurrentLocation());
-
-
         return view;
     }
-
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        // Check for location permission
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        }
 
         // Example usage of the WISC ID in the map logic
         // Set the camera to your specified coordinates
         LatLng initialLocation = new LatLng(43.074527, -89.4052735);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(initialLocation, 15));
-
-        // Here, you can use the wiscId to load specific map data or settings for the user
     }
 
-    private void performSearch(String locationName) {
-        Geocoder geocoder = new Geocoder(getContext());
-        try {
-            List<Address> addresses = geocoder.getFromLocationName(locationName, 1);
-            if (addresses != null && !addresses.isEmpty()) {
-                Address address = addresses.get(0);
-                LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission was granted, enable the location layer
+                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    mMap.setMyLocationEnabled(true);
+                }
             } else {
-                // Handle case where no address is found
+                // Permission was denied
+                Toast.makeText(getContext(), "Permission denied", Toast.LENGTH_SHORT).show();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
-
-    private void centerMapOnCurrentLocation() {
-        // Check for location permission
-        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            mMap.setMyLocationEnabled(true);
-            // Get current location and move camera
-            // This is a simplified approach. In a real app, you should use FusedLocationProviderClient
-            // or similar to get accurate location.
-            LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            if (location != null) {
-                LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15));
-            }
-        } else {
-            // Request location permission
-        }
-    }
-    }
+}
